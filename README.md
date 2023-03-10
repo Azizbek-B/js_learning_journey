85120390aeb9e359713056241822be8c0e130c83
ZED <azizbekprog.01@gmail.com>
2023-03-10 16:41:14 +0500

    variables

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 
 <h1>This is a simple repo to track the proccess of learnining the JavaScript</h1>
+<blockquote>
+A simple summary of gained knowledges
+</blockquote>
 <h2>
     Resources are used:
 </h2>
---
+++ variables/alert.js
@@ -0,0 +1,43 @@
+
+let message;
+
+message = "Hello";
+
+// Optionally we can declare variable and assign it at the same time, insted of the code above.
+/*
+Example: 
+let message = "Hello";
+*/
+
+alert(message);
+
+
+let name = 'Johnn';
+let age = '25';
+let message2 = 'World';
+
+ 
+// Alternatively we can assign multiple variable leke an example below:
+/*
+let name = 'John',
+    age = '25',
+    message2 = 'World'; 
+*/
+
+
+// !!! In the older version of script you usually will see "var" key instead of "let" key of declaration of variable
+
+/*
+ var message3 = "Hello, world";
+
+ However, as mentioned earlier, it is not commot to see using of it in modern code sample;
+*/
+
+
+// Variable is mutable, and can change it's value by reassigning it.
+
+
+message='World';
+// the previous value of variable "message" was "Hello", now it's a "World"
+
+alert(message)
