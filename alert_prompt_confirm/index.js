8f4aaec8a2e29763e6f46f0c84bc9d6f94d21e88
ZED <azizbekprog.01@gmail.com>
2023-03-14 18:31:55 +0500

    alert_prompt_confirm

---
+++ alert_prompt_confirm/index.js
@@ -0,0 +1,13 @@
+// Alert is a function with what we can notify users 
+
+// prompt  is a function that allows user to input a text
+let result = prompt("write something");
+
+alert(result);
+
+// confirm fubction shows a window on the browser with yok and cancel button
+let confirmation = confirm("press one of the buttons");
+
+alert(confirmation)
+
+// confirm returns true if user presses ok and false if cancel
